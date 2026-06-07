8927a1f4a54f075a69f5c56c10e629dea8dafa86
ravitendu <ravitendu7390@gmail.com>
2026-06-07 22:14:11 +0530

    Remove Deep Learning support

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import openpyxl
-from flask import Flask, render_template, request, Response, redirect, url_for, send_from_directory
-
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 from utils.User_input import user_input_handling
 
@@ -12,56 +10,56 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/user_input', methods=['GET','POST'])
-# getting user input from front end
+
+@app.route('/user_input', methods=['GET', 'POST'])
 def user_input():
+
     sgRNA = request.form.get('sgRNA')
     target_DNA = request.form.get('target_DNA')
     filename = request.files['filename']
-    model = request.form.get('model')
+
+    # Machine Learning is the only model now
+    model = "Machine Learning"
 
     if target_DNA:
         user_input_handling(sgRNA, target_DNA, model)
 
-    if not target_DNA:
+    else:
         str1 = ""
         contents = filename.read()
         data = contents.splitlines()
-        for i in range(1,len(data)):
+
+        for i in range(1, len(data)):
             str1 += data[i].decode()
+
         user_input_handling(sgRNA, str1, model)
-    return redirect(url_for('show_result', type=model))
+
+    return redirect(url_for('show_result'))
+
 
 @app.route('/result', methods=['GET', 'POST'])
 def show_result():
-    model = request.args.get('type')
-    arr = []
 
-    if model == 'Machine Learning':
-        file = './utils/i-CRISPR(ML)_result.csv'
-    
-    elif model == 'Deep Learning':
-        file = './utils/i-CRISPR(DL)_result.xlsx'
-        output_file = './utils/temp.csv'
-        df = pd.read_excel(file, header=None)
-        df.to_csv(output_file, index=False, header=False)
-        file = './utils/temp.csv'
+    file = './utils/i-CRISPR(ML)_result.csv'
 
     user_data = pd.read_csv(file, header=None).to_dict('index')
-    arr = list(user_data.values())[1:]    
-    return render_template('result.html', data=arr, type=model)
+    arr = list(user_data.values())[1:]
 
-@app.route("/download/<type>")
-def download_file(type):
-    print(type)
+    return render_template('result.html',
+                           data=arr,
+                           type="Machine Learning")
+
+
+@app.route("/download")
+def download_file():
+
     path = './utils'
+    file_name = 'i-CRISPR(ML)_result.csv'
 
-    if type == 'Deep Learning':
-        file_name = 'i-CRISPR(DL)_result.csv'
-    elif type == "Machine Learning":
-        file_name = 'i-CRISPR(ML)_result.csv'
-    
-    return send_from_directory(path, file_name, as_attachment=True)
+    return send_from_directory(path,
+                               file_name,
+                               as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- utils/User_input.py
+++ utils/User_input.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 from utils.Machine_learning_Backend_codes.Feature_matrix_gen import run as ml_run1
 from utils.Machine_learning_Backend_codes.RFC_output_generation import run2 as ml_run2
-from utils.Deep_learning_Backend_codes.Data_encoding import run as dl_run1
-from utils.Deep_learning_Backend_codes.FNN_output_generation import run2 as dl_run2
+
 
 def user_input_handling(SgRNA, target_DNA, model):
     a = SgRNA
@@ -24,9 +23,6 @@ def user_input_handling(SgRNA, target_DNA, model):
     df.to_excel('utils/input_data.xlsx', index=False)
     df.to_csv('utils/input_data.csv', index=False)
 
-    if model == 'Machine Learning':
-        ml_run1()
-        ml_run2()
-    elif model == 'Deep Learning':
-        dl_run1()
-        dl_run2()
+
+    ml_run1()
+    ml_run2()
